e2faea052123aac0feddba1b57d889f1be949dcc
stefobark <steve@sphinxsearch.com>
2014-10-26 08:43:27 -0700

    more info to user view

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -5,5 +5,9 @@
 <div class="row">
 <div class="col-md-4">
 
-    <p>This is user {{ $user->name }}. Email is {{ $user->email }}</p>
+    <p>This is user <strong> {{ $user->name }} </strong>. Email is <strong>{{ $user->email }}</strong></p>
+    <p>Their username is <strong>{{ $user->username }}</strong>. They live in <strong>{{ $user->city }}</strong> 
+    , {{ $user->state  }} and they're {{ $user->age }} years old. Their zipcode is {{ $user->zip }}
+     and their street address is {{ $user->address }}. AND!! their lat/long is {{ $user->lat_long }} -- so, they are in legislative district: {{ $user->leg_district }}</p>
+	 <br /> THE PASSWORD HASH IS: {{ $user->password }} <br /><br />
 @stop
