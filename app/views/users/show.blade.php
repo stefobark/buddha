15bd3374fa89032ea8e025d9ab7b5acb5293329d
stefobark <steve@sphinxsearch.com>
2014-11-02 06:50:46 -0800

    got rid of useless tables

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -4,7 +4,6 @@
 <div class="container" style="margin-top:75px">
 <div class="row">
 <div class="col-md-4">
-
     <p>This is user <strong> {{ $user->name }} </strong>.<br /> 
     Email is <strong>{{ $user->email }}</strong></p>
     <p>Their username is <strong>{{ $user->username }}</strong> . <br />
@@ -12,7 +11,7 @@
     So, they are in legislative district: {{ $user->leg_district }}</p>
 	 <br /> THE PASSWORD HASH IS: {{ $user->password }} <br /><br />
 	 <pre> {{ var_dump($q) }} </pre>
-	 <pre> {{ var_dump($l_primary) }} </pre>
+	  <pre> {{ var_dump($l_primary) }} </pre>
 	  <pre> {{ var_dump($u_primary) }} </pre>
 	  
 	  <pre> {{ var_dump($test) }} </pre>
