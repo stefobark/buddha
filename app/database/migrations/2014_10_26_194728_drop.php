96a7073f6af43aea53b97f93b53e498ce417a203
stefobark <steve@sphinxsearch.com>
2014-12-19 10:42:25 -0800

    make migrations work

---
+++ app/database/migrations/2014_10_26_194728_drop.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Drop extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table)
+{
+    $table->dropColumn('city');
+    $table->dropColumn('state');
+    $table->dropColumn('zip');
+    $table->dropColumn('address');
+});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table)
+{
+    $table->string('city');
+    $table->string('state');
+    $table->integer('zip');
+    $table->string('address');
+});
+	}
+
+}
