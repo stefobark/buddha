b660865e0d0e90b10506fe17158bd348e8dd8e2c
stefobark <steve@sphinxsearch.com>
2014-12-19 11:06:03 -0800

    fiddlin

--- app/database/migrations/2014_10_11_145856_create_users_table.php
+++ app/database/migrations/2014_10_11_145856_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration {
     {
         $table->increments('id');
         $table->string('email')->unique();
-        $table->string('name');
+        $table->string('username');
         $table->timestamps();
         
     });
