1419cd214a71999c61ea60c30b6c18787471f941
stefobark <steve@sphinxsearch.com>
2014-12-20 08:24:44 -0800

    try avg

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -83,7 +83,7 @@ class UsersController extends \BaseController {
 		$dhamma = DB::table('dhammapadas')->where('id', "$rand")->get();
 		$fourTruth->save();
 		
-		$oneAvg = DB::raw("select avg(one) from four_truths where uid=$id");
+		$oneAvg = DB::table('four_truths')->avg('one')->where('uid', "$id");
 		
 		return View::make('users.show', array('user' => $user, 'dhamma' => $dhamma, 'rand1' => $rand1, 'rand2' => $rand2, 'rand3' => $rand3, 'rand4' => $rand4, 'oneAvg' => $oneAvg));
 	}
