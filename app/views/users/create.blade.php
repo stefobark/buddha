d037e7f4d19c6dbf0482fe93be1f00a7bfcd8779
stefobark <steve@sphinxsearch.com>
2014-12-24 07:37:05 -0800

    messing with form

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -5,11 +5,22 @@
 		<img src="../img/dhammakaya-pagoda.jpg" style="width:100%"></img>
 		<div class="container" style="position:absolute;top: 100px; left: 100px; ">
 			<div class="row">
-			{{ Form::model($users, ['route' => 'users.store', 'class' => 'clearfix'])}}
-				{{ Form::text('username', $username, array('class' => 'form-control', 'placeholder' => 'Username')) }}
-				{{ Form::email('email', $email, array('class' => 'form-control', 'placeholder' => 'Email')) }}
-				{{ Form::password('password', $password, array('class' => 'form-control', 'placeholder' => 'Password')) }}
-				{{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}
+			{{ Form::model($users, ['route' => 'users.store'])}}
+			<div>
+				{{ Form::label('username', 'User Name: ') }}
+				{{ Form::text('username') }}
+			</div>
+			<div>
+				{{ Form::label('password', 'Password') }}
+				{{ Form::password('password') }}
+			</div>
+			<div>
+				{{ Form::label('email', 'Email: ') }}
+				{{ Form::email('email') }}
+			</div>
+			<div>
+				{{ Form::submit('Create Account') }}
+			</div>
 			{{ Form::close() }}
 			</div>
 		</div>
