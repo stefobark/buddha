2b17c07d297fa79f81b189df50c40a114830479b
stefobark <steve@sphinxsearch.com>
2014-12-24 07:30:47 -0800

    messing with form

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -6,21 +6,10 @@
 		<div class="container" style="position:absolute;top: 100px; left: 100px; ">
 			<div class="row">
 			{{ Form::model($users, ['route' => 'users.store', 'class' => 'clearfix'])}}
-			<div>
-				{{ Form::label('username', 'User Name: ') }}
 				{{ Form::text('username', array('class' => 'form-control', 'placeholder' => 'Username')) }}
-			</div>
-			<div>
-				{{ Form::label('password', 'Password') }}
-				{{ Form::password, array('class' => 'form-control', 'placeholder' => 'Password') }}
-			</div>
-			<div>
-				{{ Form::label('email', 'Email: ') }}
 				{{ Form::email('email', '', array('class' => 'form-control', 'placeholder' => 'Email')) }}
-			</div>
-			<div>
-				{{ Form::submit('Create Account', array('class' => 'btn btn-primary')) }}
-			</div>
+				{{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
+				{{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}
 			{{ Form::close() }}
 			</div>
 		</div>
